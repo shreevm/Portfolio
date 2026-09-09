2e30ce89ac587d82f9e7238acd9d8bb36f6a849e
shreevm <shreevenkat2002@gmail.com>
2026-09-09 11:04:17 -0400

    commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
+
 import type {Metadata} from 'next';
 import { Space_Grotesk, JetBrains_Mono, Playfair_Display } from 'next/font/google';
-import './globals.css';
+import 'app/globals.css';
 
 const spaceGrotesk = Space_Grotesk({
   subsets: ['latin'],
